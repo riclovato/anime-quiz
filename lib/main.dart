8a736685c8d4d76469b14e601e9345e1bee73559
riclovato <riic.lovato@gmail.com>
2025-09-04 17:02:39 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Homepage());
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Anime Quiz'),
+        ),
+        body: const Center(
+          child: Text('Questions'),
+        ),
+      ),
+    );
+  }
+}
